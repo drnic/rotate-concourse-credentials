a2119baa32a8ea838a37c3c7af7f4cf33d0e9b6e
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-23 12:12:35 -0700

    initial rotate.sh gets list of pipeline names

---
+++ rotate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+flytarget=$1; shift
+if [[ "${flytarget}X" == "X" ]]; then
+  echo "USAGE: ./rotate.sh <fly target alias>"
+  exit 1
+fi
+
+api=$(cat ~/.flyrc| yaml2json | jq -r ".targets.${flytarget}.api")
+username=$(cat ~/.flyrc| yaml2json | jq -r ".targets.${flytarget}.username")
+password=$(cat ~/.flyrc| yaml2json | jq -r ".targets.${flytarget}.password")
+
+echo Fetching list of pipelines...
+pipelines=$(curl -s -k -u ${username}:${password} ${api}/api/v1/pipelines | jq -r ".[].name")
+for pipeline in ${pipelines[@]}; do
+  echo $pipeline
+done

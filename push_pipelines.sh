31b3ccc002895dfe1278748dc26635087117df47
Dr Nic Williams <drnicwilliams@gmail.com>
2015-10-23 12:21:54 -0700

    script to push modified pipelines

---
+++ push_pipelines.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+flytarget=$1; shift
+if [[ "${flytarget}X" == "X" ]]; then
+  echo "USAGE: ./rotate.sh <fly target alias>"
+  exit 1
+fi
+
+target_dir=${target_dir:-tmp}
+cd ${target_dir}
+
+echo Pushing pipelines...
+for pipeline_file in $(ls *.yml); do
+  pipeline=${pipeline_file%%.*}
+  echo Pushing ${pipeline}...
+  fly -t ${flytarget} c -c ${pipeline_file} ${pipeline}
+done
